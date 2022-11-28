83c276ad698eebe0ce9802e53b7c84cfb6af2590
gmiserani <gabrielamiserani@uol.com.br>
2022-11-27 23:32:39 -0300

    Implementacao distancias EUclidiana e Manhattan

--- DotsGen.py
+++ DotsGen.py
@@ -10,13 +10,38 @@ def genDots(size):
         x = np.append(x, np.random.random_integers(0,400))
         x = np.unique(x)
     return x,y
+
+def Euclidiana(x, y, matrix, size):
+    for i in range(size):
+        for j in range(size):
+            if(i == j):
+                dist = np.inf
+            else:
+                dist = np.sqrt(((x[i]-x[j])**2)+((y[i]-y[j])**2))
+                dist = abs(dist)
+            matrix[i][j] = dist
+            matrix[j][i] = dist
+    return matrix
+
+def Manhattan(x, y, matrix, size):
+    for i in range(size):
+        for j in range(size):
+            if(i == j):
+                dist = np.inf
+            else:
+                dist = abs(x[i] - x[j]) + abs(y[i] - y[j])
+            matrix[i][j] = dist
+            matrix[j][i] = dist
+    return matrix
+
 def matrixgen(x, y, size):
     matrix=[]
     for i in range(size):
-        matrix.append([1 for i in range(size)])
-    for j in range(size):
-        matrix[j][j] = 0
+        matrix.append([0 for i in range(size)])
     return matrix
 
 x, y= genDots(4)
-matrixgen(x, y, 4)
+matrix = matrixgen(x, y, 4)
+print(Manhattan(x, y, matrix, 4))
+
+#distancia euclidiana vs distancia de manhattan
